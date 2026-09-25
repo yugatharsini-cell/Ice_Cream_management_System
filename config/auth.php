938c7da9820ea3860d72d940d4ebf09fda319f18
yugatharsini-cell <yugatharsini@gmail.com>
2026-09-25 08:51:47 +0530

    Updated project

--- config/auth.php
+++ config/auth.php
@@ -50,17 +50,19 @@ function require_admin()
 }
 
 
-
+// Admin function alias
 function requireAdmin()
 {
     require_admin();
 }
 
+
+// Get image URL
 function get_image_url($image, $name = '')
 {
     $image = trim($image ?? '');
 
-    
+    // Default image mapping
     if ($image === '') {
         $nameMap = [
             'Classic Vanilla'  => 'ClassicVanilla.jpg',
@@ -69,40 +71,59 @@ function get_image_url($image, $name = '')
             'Mango Magic'      => 'MangoMagic.webp',
             'Royal Sundae'     => 'RoyalSundae.jpg',
         ];
+
         $image = $nameMap[$name] ?? '';
+
         if ($image === '') {
             return '';
         }
     }
-s
-    if (preg_match('#^(https?:)?//#i', $image) || strpos($image, 'data:image') === 0) {
+
+
+    // Return URL or base64 image directly
+    if (
+        preg_match('#^(https?:)?//#i', $image)
+        || strpos($image, 'data:image') === 0
+    ) {
         return $image;
     }
 
-    
+
+    // Convert Windows backslashes to forward slashes
     $image = str_replace('\\', '/', $image);
 
-    
+
+    // Check whether current PHP file is inside a subdirectory
     $isSubdir = false;
+
     if (isset($_SERVER['SCRIPT_FILENAME'])) {
         $scriptDir = dirname($_SERVER['SCRIPT_FILENAME']);
         $isSubdir = (basename($scriptDir) !== 'ice_cream_shop');
     }
+
     $basePrefix = $isSubdir ? '../' : '';
 
-    
+
+    // Remove project folder from image path
     if (strpos($image, '/ice_cream_shop/') === 0) {
         $image = substr($image, strlen('/ice_cream_shop/'));
     }
 
+
+    // If path already starts with assets/
     if (strpos($image, 'assets/') === 0) {
         return $basePrefix . $image;
     }
+
+
+    // If path starts with /assets/
     if (strpos($image, '/assets/') === 0) {
         return $basePrefix . ltrim($image, '/');
     }
 
+
+    // Default image location
     return $basePrefix . 'assets/images/' . ltrim($image, '/');
 }
-
 ?>
+
